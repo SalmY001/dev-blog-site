427ae187f98b11dd0882ce5fd7d7e91a54bc51d7
SalmY001 <80605132+SalmY001@users.noreply.github.com>
2023-06-04 21:33:20 +0100

    remove homepage link

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { Blog, User } = require("../models");
 const withAuth = require("../utils/withAuth");
 
-router.get("/homepage", async(req, res) => {
+router.get("/", async(req, res) => {
     res.render("homepage");
 });
 
